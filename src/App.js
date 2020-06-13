ecf8f185c4db855a9b1123fbd37fa0084239fc66
Hamza Liaqat <hamzaliaqat1481@gmail.com>
2020-06-14 03:50:56 +0500

    3rd commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
         <h1>Hamza Liaqat</h1>
-        <h3>Aampir pinger is great teacher</h3>
+        <h3>Aamir pinger is great teacher</h3>
       <p>PIAIC Student of AI, CNC, Blockchain Batch 4 faislabad</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
